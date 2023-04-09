faa9f9050aee58ddfc50233e70d29ee19e16d513
mastermines1 <milespatrickcampbell@gmail.com>
2023-04-09 18:36:37 +0000

    fixed designer

--- NEA mini/Form1.Designer.cs
+++ NEA mini/Form1.Designer.cs
@@ -28,12 +28,413 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            picHudson = new PictureBox();
+            picRoadBack = new PictureBox();
+            tmrSwerv = new System.Windows.Forms.Timer(components);
+            tmrSwerving = new System.Windows.Forms.Timer(components);
+            tmrFall = new System.Windows.Forms.Timer(components);
+            picWakeyWines = new PictureBox();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            pictureBox5 = new PictureBox();
+            pictureBox6 = new PictureBox();
+            pictureBox7 = new PictureBox();
+            pictureBox8 = new PictureBox();
+            pictureBox9 = new PictureBox();
+            pictureBox10 = new PictureBox();
+            pictureBox11 = new PictureBox();
+            pictureBox12 = new PictureBox();
+            pictureBox13 = new PictureBox();
+            pictureBox14 = new PictureBox();
+            pictureBox15 = new PictureBox();
+            pictureBox16 = new PictureBox();
+            pictureBox17 = new PictureBox();
+            pictureBox18 = new PictureBox();
+            pictureBox19 = new PictureBox();
+            pictureBox20 = new PictureBox();
+            pictureBox21 = new PictureBox();
+            pictureBox22 = new PictureBox();
+            pictureBox23 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)picHudson).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)picRoadBack).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)picWakeyWines).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox9).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox10).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox11).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox12).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox13).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox14).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox15).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox16).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox17).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox18).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox19).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox20).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox21).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox22).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox23).BeginInit();
+            SuspendLayout();
+            // 
+            // picHudson
+            // 
+            picHudson.Image = (Image)resources.GetObject("picHudson.Image");
+            picHudson.Location = new Point(481, 375);
+            picHudson.Name = "picHudson";
+            picHudson.Size = new Size(45, 79);
+            picHudson.SizeMode = PictureBoxSizeMode.StretchImage;
+            picHudson.TabIndex = 0;
+            picHudson.TabStop = false;
+            // 
+            // picRoadBack
+            // 
+            picRoadBack.BackColor = Color.Black;
+            picRoadBack.Location = new Point(-4, 61);
+            picRoadBack.Name = "picRoadBack";
+            picRoadBack.Size = new Size(997, 308);
+            picRoadBack.TabIndex = 1;
+            picRoadBack.TabStop = false;
+            // 
+            // tmrSwerv
+            // 
+            tmrSwerv.Enabled = true;
+            tmrSwerv.Interval = 4000;
+            tmrSwerv.Tick += tmrSwerv_Tick;
+            // 
+            // tmrSwerving
+            // 
+            tmrSwerving.Interval = 10;
+            tmrSwerving.Tick += tmrSwerving_Tick;
+            // 
+            // tmrFall
+            // 
+            tmrFall.Enabled = true;
+            tmrFall.Interval = 10;
+            tmrFall.Tick += tmrFall_Tick;
+            // 
+            // picWakeyWines
+            // 
+            picWakeyWines.BackgroundImage = (Image)resources.GetObject("picWakeyWines.BackgroundImage");
+            picWakeyWines.Image = (Image)resources.GetObject("picWakeyWines.Image");
+            picWakeyWines.Location = new Point(0, 0);
+            picWakeyWines.Name = "picWakeyWines";
+            picWakeyWines.Size = new Size(982, 64);
+            picWakeyWines.SizeMode = PictureBoxSizeMode.StretchImage;
+            picWakeyWines.TabIndex = 2;
+            picWakeyWines.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.White;
+            pictureBox1.Location = new Point(-4, 202);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(989, 22);
+            pictureBox1.TabIndex = 3;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackColor = Color.White;
+            pictureBox2.Location = new Point(0, 291);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(55, 10);
+            pictureBox2.TabIndex = 3;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.BackColor = Color.White;
+            pictureBox3.Location = new Point(91, 291);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(55, 10);
+            pictureBox3.TabIndex = 3;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.BackColor = Color.White;
+            pictureBox4.Location = new Point(182, 291);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(55, 10);
+            pictureBox4.TabIndex = 3;
+            pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.BackColor = Color.White;
+            pictureBox5.Location = new Point(273, 291);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(55, 10);
+            pictureBox5.TabIndex = 3;
+            pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.BackColor = Color.White;
+            pictureBox6.Location = new Point(364, 291);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(55, 10);
+            pictureBox6.TabIndex = 3;
+            pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            pictureBox7.BackColor = Color.White;
+            pictureBox7.Location = new Point(455, 291);
+            pictureBox7.Name = "pictureBox7";
+            pictureBox7.Size = new Size(55, 10);
+            pictureBox7.TabIndex = 3;
+            pictureBox7.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            pictureBox8.BackColor = Color.White;
+            pictureBox8.Location = new Point(546, 291);
+            pictureBox8.Name = "pictureBox8";
+            pictureBox8.Size = new Size(55, 10);
+            pictureBox8.TabIndex = 3;
+            pictureBox8.TabStop = false;
+            // 
+            // pictureBox9
+            // 
+            pictureBox9.BackColor = Color.White;
+            pictureBox9.Location = new Point(637, 291);
+            pictureBox9.Name = "pictureBox9";
+            pictureBox9.Size = new Size(55, 10);
+            pictureBox9.TabIndex = 3;
+            pictureBox9.TabStop = false;
+            // 
+            // pictureBox10
+            // 
+            pictureBox10.BackColor = Color.White;
+            pictureBox10.Location = new Point(728, 291);
+            pictureBox10.Name = "pictureBox10";
+            pictureBox10.Size = new Size(55, 10);
+            pictureBox10.TabIndex = 3;
+            pictureBox10.TabStop = false;
+            // 
+            // pictureBox11
+            // 
+            pictureBox11.BackColor = Color.White;
+            pictureBox11.Location = new Point(819, 291);
+            pictureBox11.Name = "pictureBox11";
+            pictureBox11.Size = new Size(55, 10);
+            pictureBox11.TabIndex = 3;
+            pictureBox11.TabStop = false;
+            // 
+            // pictureBox12
+            // 
+            pictureBox12.BackColor = Color.White;
+            pictureBox12.Location = new Point(910, 291);
+            pictureBox12.Name = "pictureBox12";
+            pictureBox12.Size = new Size(55, 10);
+            pictureBox12.TabIndex = 3;
+            pictureBox12.TabStop = false;
+            // 
+            // pictureBox13
+            // 
+            pictureBox13.BackColor = Color.White;
+            pictureBox13.Location = new Point(0, 115);
+            pictureBox13.Name = "pictureBox13";
+            pictureBox13.Size = new Size(55, 10);
+            pictureBox13.TabIndex = 3;
+            pictureBox13.TabStop = false;
+            // 
+            // pictureBox14
+            // 
+            pictureBox14.BackColor = Color.White;
+            pictureBox14.Location = new Point(91, 115);
+            pictureBox14.Name = "pictureBox14";
+            pictureBox14.Size = new Size(55, 10);
+            pictureBox14.TabIndex = 3;
+            pictureBox14.TabStop = false;
+            // 
+            // pictureBox15
+            // 
+            pictureBox15.BackColor = Color.White;
+            pictureBox15.Location = new Point(455, 115);
+            pictureBox15.Name = "pictureBox15";
+            pictureBox15.Size = new Size(55, 10);
+            pictureBox15.TabIndex = 3;
+            pictureBox15.TabStop = false;
+            // 
+            // pictureBox16
+            // 
+            pictureBox16.BackColor = Color.White;
+            pictureBox16.Location = new Point(182, 115);
+            pictureBox16.Name = "pictureBox16";
+            pictureBox16.Size = new Size(55, 10);
+            pictureBox16.TabIndex = 3;
+            pictureBox16.TabStop = false;
+            // 
+            // pictureBox17
+            // 
+            pictureBox17.BackColor = Color.White;
+            pictureBox17.Location = new Point(546, 115);
+            pictureBox17.Name = "pictureBox17";
+            pictureBox17.Size = new Size(55, 10);
+            pictureBox17.TabIndex = 3;
+            pictureBox17.TabStop = false;
+            // 
+            // pictureBox18
+            // 
+            pictureBox18.BackColor = Color.White;
+            pictureBox18.Location = new Point(273, 115);
+            pictureBox18.Name = "pictureBox18";
+            pictureBox18.Size = new Size(55, 10);
+            pictureBox18.TabIndex = 3;
+            pictureBox18.TabStop = false;
+            // 
+            // pictureBox19
+            // 
+            pictureBox19.BackColor = Color.White;
+            pictureBox19.Location = new Point(637, 115);
+            pictureBox19.Name = "pictureBox19";
+            pictureBox19.Size = new Size(55, 10);
+            pictureBox19.TabIndex = 3;
+            pictureBox19.TabStop = false;
+            // 
+            // pictureBox20
+            // 
+            pictureBox20.BackColor = Color.White;
+            pictureBox20.Location = new Point(364, 115);
+            pictureBox20.Name = "pictureBox20";
+            pictureBox20.Size = new Size(55, 10);
+            pictureBox20.TabIndex = 3;
+            pictureBox20.TabStop = false;
+            // 
+            // pictureBox21
+            // 
+            pictureBox21.BackColor = Color.White;
+            pictureBox21.Location = new Point(728, 115);
+            pictureBox21.Name = "pictureBox21";
+            pictureBox21.Size = new Size(55, 10);
+            pictureBox21.TabIndex = 3;
+            pictureBox21.TabStop = false;
+            // 
+            // pictureBox22
+            // 
+            pictureBox22.BackColor = Color.White;
+            pictureBox22.Location = new Point(819, 115);
+            pictureBox22.Name = "pictureBox22";
+            pictureBox22.Size = new Size(55, 10);
+            pictureBox22.TabIndex = 3;
+            pictureBox22.TabStop = false;
+            // 
+            // pictureBox23
+            // 
+            pictureBox23.BackColor = Color.White;
+            pictureBox23.Location = new Point(910, 115);
+            pictureBox23.Name = "pictureBox23";
+            pictureBox23.Size = new Size(55, 10);
+            pictureBox23.TabIndex = 3;
+            pictureBox23.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(0, 192, 0);
+            ClientSize = new Size(982, 478);
+            Controls.Add(pictureBox23);
+            Controls.Add(pictureBox12);
+            Controls.Add(pictureBox22);
+            Controls.Add(pictureBox11);
+            Controls.Add(pictureBox21);
+            Controls.Add(pictureBox10);
+            Controls.Add(pictureBox20);
+            Controls.Add(pictureBox6);
+            Controls.Add(pictureBox19);
+            Controls.Add(pictureBox9);
+            Controls.Add(pictureBox18);
+            Controls.Add(pictureBox5);
+            Controls.Add(pictureBox17);
+            Controls.Add(pictureBox8);
+            Controls.Add(pictureBox16);
+            Controls.Add(pictureBox4);
+            Controls.Add(pictureBox15);
+            Controls.Add(pictureBox14);
+            Controls.Add(pictureBox7);
+            Controls.Add(pictureBox13);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Controls.Add(picWakeyWines);
+            Controls.Add(picHudson);
+            Controls.Add(picRoadBack);
+            Name = "Form1";
+            ShowIcon = false;
+            KeyPress += Form1_KeyPress;
+            ((System.ComponentModel.ISupportInitialize)picHudson).EndInit();
+            ((System.ComponentModel.ISupportInitialize)picRoadBack).EndInit();
+            ((System.ComponentModel.ISupportInitialize)picWakeyWines).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox9).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox10).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox11).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox12).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox13).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox14).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox15).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox16).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox17).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox18).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox19).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox20).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox21).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox22).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox23).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private PictureBox picHudson;
+        private PictureBox picRoadBack;
+        private System.Windows.Forms.Timer tmrSwerv;
+        private System.Windows.Forms.Timer tmrSwerving;
+        private System.Windows.Forms.Timer tmrFall;
+        private PictureBox picWakeyWines;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox5;
+        private PictureBox pictureBox6;
+        private PictureBox pictureBox7;
+        private PictureBox pictureBox8;
+        private PictureBox pictureBox9;
+        private PictureBox pictureBox10;
+        private PictureBox pictureBox11;
+        private PictureBox pictureBox12;
+        private PictureBox pictureBox13;
+        private PictureBox pictureBox14;
+        private PictureBox pictureBox15;
+        private PictureBox pictureBox16;
+        private PictureBox pictureBox17;
+        private PictureBox pictureBox18;
+        private PictureBox pictureBox19;
+        private PictureBox pictureBox20;
+        private PictureBox pictureBox21;
+        private PictureBox pictureBox22;
+        private PictureBox pictureBox23;
     }
 }
--- NEA mini/Form1.cs
+++ NEA mini/Form1.cs
@@ -11,22 +11,8 @@ namespace NEA_mini
         }
 
 
-
-        private void tmrFallTime_Tick(object sender, EventArgs e)
-        {
-            if (picHudson.Top < 400)//dont fall off screen
-            {
-                picHudson.Top += 1;
-
-            }
-            if (picHudson.Top < 75)
-            {//victory?
-
-            }
-        }
-
         private void tmrSwerv_Tick(object sender, EventArgs e)
-          {
+        {
             Random random = new Random();
             tmrSwerv.Interval = random.Next(4000, 6000);
             tmrSwerving.Enabled = true;
@@ -62,6 +48,19 @@ namespace NEA_mini
             if (e.KeyChar == 32)
             {
                 picHudson.Top -= 20;
+            }
+        }
+
+        private void tmrFall_Tick(object sender, EventArgs e)
+        {
+            if (picHudson.Top < 400)//dont fall off screen
+            {
+                picHudson.Top += 1;
+
+            }
+            if (picHudson.Top < 75)
+            {//victory?
+
             }
         }
     }
