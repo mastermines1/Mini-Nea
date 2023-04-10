6c7f5ba7f0feed5ec7e0622df599e12e784950a6
Miles Campbell <MilesPatrickCampbell@gmail.com>
2023-04-10 22:06:04 +0100

    working on car class

--- NEA mini/Form1.cs
+++ NEA mini/Form1.cs
@@ -1,3 +1,5 @@
+using System.Runtime.CompilerServices;
+
 namespace NEA_mini
 {
     public partial class Form1 : Form
@@ -63,6 +65,83 @@ namespace NEA_mini
             {
                 picHudson.Top -= 20;
             }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+
+            car Car1 = new car(false,true);
+            this.Controls.Add(Car1.picCar);
+            Car1.picCar.BringToFront();
+        }
+
+        private void tmrEnemySpawn_Tick(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+        private void tmrCarMove_Tick(object sender, EventArgs e)
+        {
+            
+        }
+    }
+    public class car
+    {
+        public bool travelsLeft { get; }
+        public PictureBox picCar { get; }
+        public System.Windows.Forms.Timer Timer { get; }
+        public bool fastLane { get; }
+
+        public car(bool left,bool fast)
+        {
+
+            this.travelsLeft = left;
+            this.fastLane = fast;
+            if(left)
+            {
+               picCar = new PictureBox
+                {
+                    Name = "picCar",
+                    Size = new Size(100, 50),
+                    Location = new Point(-100, 230),
+                    ImageLocation = "Car top down.png",
+                    SizeMode = PictureBoxSizeMode.StretchImage,
+                    BackColor = Color.Black,
+                };
+            }
+            else
+            {
+                picCar = new PictureBox
+                {
+                    Name = "picCar",
+                    Size = new Size(100, 50),
+                    Location = new Point(00, 230),
+                    ImageLocation = "Car top down.png",
+                    SizeMode = PictureBoxSizeMode.StretchImage,
+                    BackColor = Color.Black,
+                };
+            }
+            if(fast)
+            {
+                this.Timer = new System.Windows.Forms.Timer
+                {
+                    Interval = 10,
+                    Enabled = true,
+                };
+            }
+            else
+            {
+                this.Timer = new System.Windows.Forms.Timer
+                {
+                    Interval = 20,
+                    Enabled = true,
+                };
+            }    
+
         }
     }
 }
