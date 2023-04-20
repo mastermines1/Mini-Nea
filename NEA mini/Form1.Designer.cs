0bcfa12afbdc0344ac8263ebc94b7fc284880c49
mastermines1 <milespatrickcampbell@gmail.com>
2023-04-20 11:35:06 +0200

    idk stuff

--- NEA mini/Form1.Designer.cs
+++ NEA mini/Form1.Designer.cs
@@ -3,7 +3,7 @@
     partial class Form1
     {
 
-        int level, livesRemaining,score;
+        int level, livesRemaining, score;
         /// <summary>
         ///  Required designer variable.
         /// </summary>
@@ -30,138 +30,135 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.picHudson = new System.Windows.Forms.PictureBox();
-            this.tmrSwerv = new System.Windows.Forms.Timer(this.components);
-            this.tmrSwerving = new System.Windows.Forms.Timer(this.components);
-            this.tmrFall = new System.Windows.Forms.Timer(this.components);
-            this.picWakeyWines = new System.Windows.Forms.PictureBox();
-            this.tmrEnemySpawnFast = new System.Windows.Forms.Timer(this.components);
-            this.tmrEnemySpawnSlow = new System.Windows.Forms.Timer(this.components);
-            this.picReturn = new System.Windows.Forms.PictureBox();
-            this.pictureBox25 = new System.Windows.Forms.PictureBox();
-            this.lblScore = new System.Windows.Forms.Label();
-            this.tmrScore = new System.Windows.Forms.Timer(this.components);
-            ((System.ComponentModel.ISupportInitialize)(this.picHudson)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.picWakeyWines)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.picReturn)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox25)).BeginInit();
-            this.SuspendLayout();
+            picHudson = new PictureBox();
+            tmrSwerv = new System.Windows.Forms.Timer(components);
+            tmrSwerving = new System.Windows.Forms.Timer(components);
+            tmrFall = new System.Windows.Forms.Timer(components);
+            picWakeyWines = new PictureBox();
+            tmrEnemySpawnFast = new System.Windows.Forms.Timer(components);
+            tmrEnemySpawnSlow = new System.Windows.Forms.Timer(components);
+            picReturn = new PictureBox();
+            pictureBox25 = new PictureBox();
+            lblScore = new Label();
+            tmrScore = new System.Windows.Forms.Timer(components);
+            ((System.ComponentModel.ISupportInitialize)picHudson).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)picWakeyWines).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)picReturn).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox25).BeginInit();
+            SuspendLayout();
             // 
             // picHudson
             // 
-            this.picHudson.Image = ((System.Drawing.Image)(resources.GetObject("picHudson.Image")));
-            this.picHudson.Location = new System.Drawing.Point(481, 425);
-            this.picHudson.Name = "picHudson";
-            this.picHudson.Size = new System.Drawing.Size(29, 44);
-            this.picHudson.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.picHudson.TabIndex = 0;
-            this.picHudson.TabStop = false;
+            picHudson.Image = (Image)resources.GetObject("picHudson.Image");
+            picHudson.Location = new Point(481, 425);
+            picHudson.Name = "picHudson";
+            picHudson.Size = new Size(29, 44);
+            picHudson.SizeMode = PictureBoxSizeMode.StretchImage;
+            picHudson.TabIndex = 0;
+            picHudson.TabStop = false;
             // 
             // tmrSwerv
             // 
-            this.tmrSwerv.Enabled = true;
-            this.tmrSwerv.Interval = 4000;
-            this.tmrSwerv.Tick += new System.EventHandler(this.tmrSwerv_Tick);
+            tmrSwerv.Enabled = true;
+            tmrSwerv.Interval = 4000;
+            tmrSwerv.Tick += tmrSwerv_Tick;
             // 
             // tmrSwerving
             // 
-            this.tmrSwerving.Interval = 10;
-            this.tmrSwerving.Tick += new System.EventHandler(this.tmrSwerving_Tick);
+            tmrSwerving.Interval = 10;
+            tmrSwerving.Tick += tmrSwerving_Tick;
             // 
             // tmrFall
             // 
-            this.tmrFall.Enabled = true;
-            this.tmrFall.Interval = 10;
-            this.tmrFall.Tick += new System.EventHandler(this.tmrFall_Tick);
+            tmrFall.Enabled = true;
+            tmrFall.Interval = 10;
+            tmrFall.Tick += tmrFall_Tick;
             // 
             // picWakeyWines
             // 
-            this.picWakeyWines.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("picWakeyWines.BackgroundImage")));
-            this.picWakeyWines.Image = ((System.Drawing.Image)(resources.GetObject("picWakeyWines.Image")));
-            this.picWakeyWines.Location = new System.Drawing.Point(0, 0);
-            this.picWakeyWines.Name = "picWakeyWines";
-            this.picWakeyWines.Size = new System.Drawing.Size(982, 64);
-            this.picWakeyWines.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.picWakeyWines.TabIndex = 2;
-            this.picWakeyWines.TabStop = false;
+            picWakeyWines.BackgroundImage = (Image)resources.GetObject("picWakeyWines.BackgroundImage");
+            picWakeyWines.Image = (Image)resources.GetObject("picWakeyWines.Image");
+            picWakeyWines.Location = new Point(0, 0);
+            picWakeyWines.Name = "picWakeyWines";
+            picWakeyWines.Size = new Size(982, 64);
+            picWakeyWines.SizeMode = PictureBoxSizeMode.StretchImage;
+            picWakeyWines.TabIndex = 2;
+            picWakeyWines.TabStop = false;
             // 
             // tmrEnemySpawnFast
             // 
-            this.tmrEnemySpawnFast.Enabled = true;
-            this.tmrEnemySpawnFast.Interval = 1;
-            this.tmrEnemySpawnFast.Tick += new System.EventHandler(this.tmrEnemySpawnFast_Tick);
+            tmrEnemySpawnFast.Enabled = true;
+            tmrEnemySpawnFast.Interval = 1;
+            tmrEnemySpawnFast.Tick += tmrEnemySpawnFast_Tick;
             // 
             // tmrEnemySpawnSlow
             // 
-            this.tmrEnemySpawnSlow.Enabled = true;
-            this.tmrEnemySpawnSlow.Interval = 1;
-            this.tmrEnemySpawnSlow.Tick += new System.EventHandler(this.tmrEnemySpawnSlow_Tick);
+            tmrEnemySpawnSlow.Enabled = true;
+            tmrEnemySpawnSlow.Interval = 1;
+            tmrEnemySpawnSlow.Tick += tmrEnemySpawnSlow_Tick;
             // 
             // picReturn
             // 
-            this.picReturn.BackColor = System.Drawing.Color.White;
-            this.picReturn.Image = ((System.Drawing.Image)(resources.GetObject("picReturn.Image")));
-            this.picReturn.Location = new System.Drawing.Point(865, 0);
-            this.picReturn.Name = "picReturn";
-            this.picReturn.Size = new System.Drawing.Size(120, 22);
-            this.picReturn.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.picReturn.TabIndex = 5;
-            this.picReturn.TabStop = false;
-            this.picReturn.Visible = false;
-            this.picReturn.Click += new System.EventHandler(this.picReturn_Click);
+            picReturn.BackColor = Color.White;
+            picReturn.Image = (Image)resources.GetObject("picReturn.Image");
+            picReturn.Location = new Point(865, 0);
+            picReturn.Name = "picReturn";
+            picReturn.Size = new Size(120, 22);
+            picReturn.SizeMode = PictureBoxSizeMode.StretchImage;
+            picReturn.TabIndex = 5;
+            picReturn.TabStop = false;
+            picReturn.Visible = false;
+            picReturn.Click += picReturn_Click;
             // 
             // pictureBox25
             // 
-            this.pictureBox25.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox25.Image")));
-            this.pictureBox25.Location = new System.Drawing.Point(0, 61);
-            this.pictureBox25.Name = "pictureBox25";
-            this.pictureBox25.Size = new System.Drawing.Size(982, 363);
-            this.pictureBox25.TabIndex = 6;
-            this.pictureBox25.TabStop = false;
+            pictureBox25.Image = (Image)resources.GetObject("pictureBox25.Image");
+            pictureBox25.Location = new Point(0, 61);
+            pictureBox25.Name = "pictureBox25";
+            pictureBox25.Size = new Size(982, 363);
+            pictureBox25.TabIndex = 6;
+            pictureBox25.TabStop = false;
             // 
             // lblScore
             // 
-            this.lblScore.AutoSize = true;
-            this.lblScore.Location = new System.Drawing.Point(12, 454);
-            this.lblScore.Name = "lblScore";
-            this.lblScore.Size = new System.Drawing.Size(0, 15);
-            this.lblScore.TabIndex = 7;
+            lblScore.AutoSize = true;
+            lblScore.Location = new Point(12, 454);
+            lblScore.Name = "lblScore";
+            lblScore.Size = new Size(0, 15);
+            lblScore.TabIndex = 7;
             // 
             // tmrScore
             // 
-            this.tmrScore.Enabled = true;
-            this.tmrScore.Interval = 500;
-            this.tmrScore.Tick += new System.EventHandler(this.tmrScore_Tick);
+            tmrScore.Enabled = true;
+            tmrScore.Interval = 500;
+            tmrScore.Tick += tmrScore_Tick;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
-            this.ClientSize = new System.Drawing.Size(982, 478);
-            this.Controls.Add(this.lblScore);
-            this.Controls.Add(this.picReturn);
-            this.Controls.Add(this.picWakeyWines);
-            this.Controls.Add(this.picHudson);
-            this.Controls.Add(this.pictureBox25);
-            this.Name = "Form1";
-            this.ShowIcon = false;
-            this.Load += new System.EventHandler(this.Form1_Load);
-            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
-            ((System.ComponentModel.ISupportInitialize)(this.picHudson)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.picWakeyWines)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.picReturn)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox25)).EndInit();
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(0, 192, 0);
+            ClientSize = new Size(982, 478);
+            Controls.Add(lblScore);
+            Controls.Add(picReturn);
+            Controls.Add(picWakeyWines);
+            Controls.Add(picHudson);
+            Controls.Add(pictureBox25);
+            Name = "Form1";
+            ShowIcon = false;
+            Load += Form1_Load;
+            KeyUp += Form1_KeyUp;
+            ((System.ComponentModel.ISupportInitialize)picHudson).EndInit();
+            ((System.ComponentModel.ISupportInitialize)picWakeyWines).EndInit();
+            ((System.ComponentModel.ISupportInitialize)picReturn).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox25).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
-
-        private PictureBox picHudson;
         private System.Windows.Forms.Timer tmrSwerv;
         private System.Windows.Forms.Timer tmrSwerving;
         private System.Windows.Forms.Timer tmrFall;
@@ -172,5 +169,6 @@
         private PictureBox pictureBox25;
         private Label lblScore;
         private System.Windows.Forms.Timer tmrScore;
+        public PictureBox picHudson;
     }
 }
--- NEA mini/Form1.cs
+++ NEA mini/Form1.cs
@@ -1,3 +1,5 @@
+using System.Globalization;
+
 namespace NEA_mini
 {
     public partial class Form1 : Form
@@ -5,7 +7,7 @@ namespace NEA_mini
 
         public int swerveCountDown = 100;
 
-        public Form1(int Lvl,int LivesRemaining,int score)
+        public Form1(int Lvl, int LivesRemaining, int score)
         {
             level = Lvl;
             this.livesRemaining = LivesRemaining;
@@ -57,13 +59,16 @@ namespace NEA_mini
             {
                 if (level < 2)
                 {
-                picHudson.Top = 420;
-                FormQuiz frmQuiz = new FormQuiz(level, 0,livesRemaining,score);
-                frmQuiz.Show(); //go to questions
-                this.Dispose();
+                    picHudson.Top = 420;
+                    FormQuiz frmQuiz = new FormQuiz(level, 0, livesRemaining, score);
+                    frmQuiz.Show(); //go to questions
+                    this.Dispose();
                 }
                 else
                 {
+                    FormBoard frmbrd = new FormBoard(score);
+                    this.Dispose();
+                    frmbrd.Show();
                     //victory
                 }
             }
@@ -79,11 +84,11 @@ namespace NEA_mini
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            lblScore.Text = "Score: "+ score;
+            lblScore.Text = "Score: " + score;
         }
-           
 
-        
+
+
 
 
         private void tmrEnemySpawnFast_Tick(object sender, EventArgs e)
@@ -98,20 +103,24 @@ namespace NEA_mini
             Car2.picCar.BringToFront();
 
         }
-         void hudsonHit()
-         {
+        public void hudsonHit()
+        {
             livesRemaining += 1;
-            if(livesRemaining == 0)
+            if (livesRemaining == 0)
             {
+                FormBoard frmbrd = new FormBoard(score);
+                this.Dispose();
+                frmbrd.Show();
                 //gameover
             }
             else
             {
-                Form1 frm = new Form1(level,livesRemaining,score);
+                Form1 frm = new Form1(level, livesRemaining, score);
                 this.Dispose();
                 frm.Show();
             }
-         }
+
+        }
 
 
         private void tmrEnemySpawnSlow_Tick(object sender, EventArgs e)
@@ -138,116 +147,121 @@ namespace NEA_mini
         private void tmrScore_Tick(object sender, EventArgs e)
         {
             score -= 10;
-            lblScore.Text = "Score: " +score;
+            lblScore.Text = "Score: " + score;
         }
     }
 }
-    public class car
+public class car
+{
+    public bool travelsLeft { get; }
+    public PictureBox picCar { get; }
+    public System.Windows.Forms.Timer tmr { get; }
+    public bool fastLane { get; }
+
+
+    public car(bool left, bool fast)
     {
-        public bool travelsLeft { get; }
-        public PictureBox picCar { get; }
-        public System.Windows.Forms.Timer tmr { get; }
-        public bool fastLane { get; }
 
-
-        public car(bool left, bool fast)
+        this.travelsLeft = left;
+        this.fastLane = fast;
+        if (left && fast)
         {
-
-            this.travelsLeft = left;
-            this.fastLane = fast;
-            if (left && fast)
+            this.picCar = new PictureBox
             {
-                this.picCar = new PictureBox
-                {
-                    Name = "picCar",
-                    Size = new Size(100, 50),
-                    Location = new Point(900, 140),
-                    ImageLocation = "Car top down.png",
-                    SizeMode = PictureBoxSizeMode.StretchImage,
-                    BackColor = Color.Black,
-                };
-                this.tmr = new System.Windows.Forms.Timer
-                {
-                    Interval = 10,
-                    Enabled = true,
-                };
-            }
-            else if (left && fast == false)
+                Name = "picCar",
+                Size = new Size(100, 50),
+                Location = new Point(900, 140),
+                ImageLocation = "Car top down.png",
+                SizeMode = PictureBoxSizeMode.StretchImage,
+                BackColor = Color.Black,
+            };
+            this.tmr = new System.Windows.Forms.Timer
             {
-                this.picCar = new PictureBox
-                {
-                    Name = "picCar",
-                    Size = new Size(100, 50),
-                    Location = new Point(900, 65),
-                    ImageLocation = "Car top down.png",
-                    SizeMode = PictureBoxSizeMode.StretchImage,
-                    BackColor = Color.Black,
-                };
-                this.tmr = new System.Windows.Forms.Timer
-                {
-                    Interval = 20,
-                    Enabled = true,
-                };
-            }
-            else if (left == false && fast)
-            {
-                this.picCar = new PictureBox
-                {
-                    Name = "picCar",
-                    Size = new Size(100, 50),
-                    Location = new Point(-100, 275),
-                    ImageLocation = "Car top down.png",
-                    SizeMode = PictureBoxSizeMode.StretchImage,
-                    BackColor = Color.Black,
-                };
-                this.tmr = new System.Windows.Forms.Timer
-                {
-                    Interval = 10,
-                    Enabled = true,
-                };
-            }
-            else if (left == false && fast == false)
-            {
-                this.picCar = new PictureBox
-                {
-                    Name = "picCar",
-                    Size = new Size(100, 50),
-                    Location = new Point(-100, 355),
-                    ImageLocation = "Car top down.png",
-                    SizeMode = PictureBoxSizeMode.StretchImage,
-                    BackColor = Color.Black,
-                };
-                this.tmr = new System.Windows.Forms.Timer
-                {
-                    Interval = 20,
-                    Enabled = true,
-                };
-            }
-            this.tmr.Tick += new EventHandler(tmr_tick);
+                Interval = 10,
+                Enabled = true,
+            };
         }
-        private void tmr_tick(Object sender, EventArgs e)
+        else if (left && fast == false)
         {
-            if (this.fastLane && this.travelsLeft == false)
+            this.picCar = new PictureBox
             {
-                picCar.Left += 10;
-            }
-            else if (this.fastLane == false && this.travelsLeft == false)
+                Name = "picCar",
+                Size = new Size(100, 50),
+                Location = new Point(900, 65),
+                ImageLocation = "Car top down.png",
+                SizeMode = PictureBoxSizeMode.StretchImage,
+                BackColor = Color.Black,
+            };
+            this.tmr = new System.Windows.Forms.Timer
             {
-                picCar.Left += 5;
-            }
-            else if (this.fastLane && this.travelsLeft == true)
-            {
-                picCar.Left -= 10;
-            }
-            else if (this.fastLane == false && this.travelsLeft == true)
-            {
-                picCar.Left -= 5;
-            }
-            if (this.picCar.Location.X > 900 || (this.picCar.Location.X < -100))
-            {
-                this.picCar.Dispose();
-                this.tmr.Dispose();
-            }
+                Interval = 20,
+                Enabled = true,
+            };
         }
-
+        else if (left == false && fast)
+        {
+            this.picCar = new PictureBox
+            {
+                Name = "picCar",
+                Size = new Size(100, 50),
+                Location = new Point(-100, 275),
+                ImageLocation = "Car top down.png",
+                SizeMode = PictureBoxSizeMode.StretchImage,
+                BackColor = Color.Black,
+            };
+            this.tmr = new System.Windows.Forms.Timer
+            {
+                Interval = 10,
+                Enabled = true,
+            };
+        }
+        else if (left == false && fast == false)
+        {
+            this.picCar = new PictureBox
+            {
+                Name = "picCar",
+                Size = new Size(100, 50),
+                Location = new Point(-100, 355),
+                ImageLocation = "Car top down.png",
+                SizeMode = PictureBoxSizeMode.StretchImage,
+                BackColor = Color.Black,
+            };
+            this.tmr = new System.Windows.Forms.Timer
+            {
+                Interval = 20,
+                Enabled = true,
+            };
+        }
+        this.tmr.Tick += new EventHandler(tmr_tick);
     }
+    private void tmr_tick(Object sender, EventArgs e)
+    {
+        if (this.fastLane && this.travelsLeft == false)
+        {
+            picCar.Left += 10;
+        }
+        else if (this.fastLane == false && this.travelsLeft == false)
+        {
+            picCar.Left += 5;
+        }
+        else if (this.fastLane && this.travelsLeft == true)
+        {
+            picCar.Left -= 10;
+        }
+        else if (this.fastLane == false && this.travelsLeft == true)
+        {
+            picCar.Left -= 5;
+        }
+        if (this.picCar.Location.X > 900 || (this.picCar.Location.X < -100))
+        {
+            this.picCar.Dispose();
+            this.tmr.Dispose();
+        }
+        Control[] i = Application.OpenForms[1].Controls.Find("picHudson",true);
+        if(picCar.Bounds.IntersectsWith(i[0].Bounds))
+        {
+            Application.OpenForms[1]
+        }
+    }
+
+}
